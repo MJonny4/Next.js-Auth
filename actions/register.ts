2d9a82498d01c68ad457c0f89734cd6a917c01ba
Ion <Ion2000m@gmail.com>
2024-02-04 17:36:58 +0100

    Add dependencies and update layout component

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server'
 
-import { RegisterSchema } from '@/schemas'
+import bcrypt from 'bcrypt'
 import * as z from 'zod'
+import { RegisterSchema } from '@/schemas'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -10,5 +13,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: 'Invalid fields' }
     }
 
-    return { success: 'Email sent' }
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Email is not taken
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: 'Email already taken' }
+    }
+
+    // Create user
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    })
+
+    // TODO: Send verification token email
+
+    return { success: 'User created' }
 }
