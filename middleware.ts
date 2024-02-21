7c2dce5a41b4fe35eacf954f6f33751d2e176a10
Ion <Ion2000m@gmail.com>
2024-02-21 17:16:28 +0100

    Update authentication configuration and add new routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
-import { auth } from '@/auth'
+import authConfig from '@/auth.config'
+import NextAuth from 'next-auth'
+
+const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
+    const isLoggedIn = !!req.auth
     console.log('ROUTE: ', req.nextUrl.pathname)
+    console.log('isLoggedIn: ', isLoggedIn)
 })
 
 // Optionally, don't invoke Middleware on some paths
