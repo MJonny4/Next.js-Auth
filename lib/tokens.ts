175e10d8c97e849f9f08684b6cbc9b6a9064737c
Ion <Ion2000m@gmail.com>
2024-02-25 13:43:09 +0100

    Fix token expiration time and add react-spinners package

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -5,7 +5,7 @@ import { v4 as uuidV4 } from 'uuid'
 export const generateVerificationToken = async (email: string) => {
     const token = uuidV4()
     // TODO: Fix this!
-    const expires = new Date().getTime() + 3600 * 1000 // 1 hour from now
+    const expires = new Date(new Date().getTime() + 5 * 60 * 1000); // 5 minutes from now
 
     const existingToken = await getVerificationTokenByEmail(email)
     if (existingToken) {
@@ -20,7 +20,7 @@ export const generateVerificationToken = async (email: string) => {
         data: {
             email,
             token,
-            expires: new Date(expires), // Convert expires to a Date object
+            expires,
         },
     })
 
